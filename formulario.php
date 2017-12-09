7bf3e12cdee247f829a4a68efaaa5f4326400d9d
{Bredgary} <Bredgary@github.com>
2017-12-09 01:17:44 -1100

    09/12/17
    actualizacion

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<?php
+ 
+    $destino = "bred.valenzuela@gmail.com";
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $asunto = $_POST["asunto"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nAsunto: " . $asunto . "\nMensaje:" . $mensaje;
+    
+    mail($destino,$asunto,$contenido);
+    header("Location:Correo.html");
+
+?>
